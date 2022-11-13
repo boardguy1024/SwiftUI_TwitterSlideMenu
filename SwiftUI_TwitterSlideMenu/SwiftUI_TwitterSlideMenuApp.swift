0d2283420e33d3c918019987b907a4136fce753c
boardguy1024 <boardguy1024@gmail.com>
2022-11-13 16:03:06 +0900

    Initial Commit

---
+++ SwiftUI_TwitterSlideMenu/SwiftUI_TwitterSlideMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TwitterSlideMenuApp.swift
+//  SwiftUI_TwitterSlideMenu
+//
+//  Created by park kyung seok on 2022/11/13.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TwitterSlideMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
